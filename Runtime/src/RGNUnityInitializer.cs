29a96d4d1ed31f93eada100d2ebdf0e697a02112
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2023-01-29 19:29:13 +0100

    New package version for io.getready.rgn.impl.firebase, version: v0.0.14.24

--- Runtime/src/RGNUnityInitializer.cs
+++ Runtime/src/RGNUnityInitializer.cs
@@ -1,9 +1,12 @@
+using RGN.Modules.SignIn;
 using UnityEngine;
 
 namespace RGN.Impl.Firebase
 {
     public class RGNUnityInitializer : MonoBehaviour
     {
+        [SerializeField] private bool _autoGuestLogin = true;
+
         private bool _initialized = false;
         private async void Awake()
         {
@@ -12,8 +15,18 @@ namespace RGN.Impl.Firebase
                 return;
             }
             RGNCoreBuilder.CreateInstance(new Dependencies());
+            RGNCore.I.AuthenticationChanged += OnAuthenticationChanged;
             await RGNCoreBuilder.BuildAsync();
             _initialized = true;
+        }
+
+        private void OnAuthenticationChanged(EnumLoginState enumLoginState, EnumLoginError error)
+        {
+            if (_autoGuestLogin && enumLoginState == EnumLoginState.NotLoggedIn)
+            {
+                Debug.Log("Automatically logging in as a guest");
+                GuestSignInModule.I.TryToSignIn();
+            }
         }
     }
 }
