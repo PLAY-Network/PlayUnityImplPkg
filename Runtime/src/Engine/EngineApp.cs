f06c755ae8952e91dd6a31978dab753d0e0921f6
Git GetReady <git@getready.io>
2023-09-14 14:35:01 +0000

    New package version for ImplFirebase, version: 0.7.0-dev.347

--- Runtime/src/Engine/EngineApp.cs
+++ Runtime/src/Engine/EngineApp.cs
@@ -34,7 +34,10 @@ namespace RGN.Impl.Firebase.Engine
         public void DestroyGameObjectWithUpdater(IRGNUpdater rgnUpdater)
         {
             var asComponent = rgnUpdater as RGNUnityUpdater;
-            Object.Destroy(asComponent?.gameObject);
+            if ( asComponent != null && asComponent.gameObject)
+            {
+                Object.Destroy(asComponent.gameObject);
+            }
         }
     }
 }
--- Runtime/src/Engine/Time.cs
+++ Runtime/src/Engine/Time.cs
@@ -4,6 +4,7 @@ namespace RGN.Impl.Firebase.Engine
 {
     public sealed class Time : ITime
     {
+        float ITime.unscaledTime => UnityEngine.Time.unscaledTime;
         float ITime.time => UnityEngine.Time.time;
 
         float ITime.timeScale { get => UnityEngine.Time.timeScale; set => UnityEngine.Time.timeScale = value; }
