3fdb1cce63d505119b0ef4d255ec657908336e55
Git GetReady <git@getready.io>
2024-03-08 21:09:03 +0000

    New package version for ImplFirebase, version: 0.9.0-dev.523

---
+++ Runtime/src/DynamicLinks/DynamicLinkReceivedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using RGN.ImplDependencies.Core.DynamicLinks;
+using FirebaseDynamicLink = Firebase.DynamicLinks.ReceivedDynamicLink;
+
+namespace RGN.Modules.DynamicLinks.Runtime
+{
+    public class DynamicLinkReceivedEventArgs : IDynamicLinkReceivedEventArgs
+    {
+        public IDynamicLink ReceivedDynamicLink { get; }
+        
+        internal DynamicLinkReceivedEventArgs(FirebaseDynamicLink receivedFirebaseDynamicLink)
+        {
+            ReceivedDynamicLink = new DynamicLink(receivedFirebaseDynamicLink);
+        }
+    }
+}
